b0dd6ec04789e09b0c5700996ed188fdf1916ee5
Kushal Jain <jkushal069@gmail.com>
2024-01-20 23:42:27 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 // import doctorsData from "./fixHealth.doctors.json";
 import db from "./config/mongoose.js";
 import { Doctor } from "./model/doctorsModel.js";
@@ -8,11 +9,18 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/api/doctors", async (req, res) => {
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     const data = await Doctor.find({});
     return res.json(data);
   } catch (error) {
@@ -26,6 +34,7 @@ app.get("/api/doctors/:id", async (req, res) => {
   try {
     // Find the doctor by ID
     const doctor = await Doctor.findById(doctorId);
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
     if (!doctor) {
       // If the doctor is not found, send a 404 Not Found response
