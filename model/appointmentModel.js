5574cf36879974b803c47e1c3c096e6fbc9c522e
Kushal Jain <jkushal069@gmail.com>
2024-02-05 23:22:56 +0530

    added tim stamps

--- model/appointmentModel.js
+++ model/appointmentModel.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
 
-const appointmentSchema = new mongoose.Schema({
-  doctor: String,
-  date: String,
-  time: Array,
-  img: String,
-  specialties: String,
-  city: String,
-  slots: Array,
-});
+const appointmentSchema = new mongoose.Schema(
+  {
+    doctor: String,
+    date: String,
+    time: Array,
+    img: String,
+    specialties: String,
+    city: String,
+    slots: Array,
+    isAllocated: {
+      type: Boolean,
+      default: false,
+      timestamps: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Appointment = mongoose.model("Appointment", appointmentSchema);
